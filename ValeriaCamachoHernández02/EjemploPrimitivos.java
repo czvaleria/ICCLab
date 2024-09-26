7f16824f1418c6a9a626236ef85f9db0712eb775
Valeria-ca <valeria.camacho@ciencias.unam.mx>
2024-09-25 21:03:50 -0600

    Práctica 02

---
+++ ValeriaCamachoHernández02/EjemploPrimitivos.java
@@ -0,0 +1,25 @@
+public class EjemploPrimitivos {
+public static void main(String[] args) {
+
+//Tipo numérico:
+int unInt = 771;
+float unFloat = 567.89f;
+double unDouble = 3.4523421;
+
+//Tipo carácter:
+char unChar = 'M';
+String unString = "¡Hola!";
+
+//Tipo booleano: 
+boolean unBool = true;
+
+//En terminal
+System.out.println("El valor de un tipo de variable int es: "+unInt);
+System.out.println("El valor de un tipo de variable float es: "+unFloat);
+System.out.println("El valor de un tipo de variable double es: "+unDouble);
+System.out.println("El valor de un tipo de variable char es: "+unChar);
+System.out.println("El valor de una String es: "+unString);
+System.out.println("El valor de un tipo de variable boolean es: "+unBool);
+
+  }
+}
---
+++ ValeriaCamachoHernández02/ReadMe.txt
@@ -0,0 +1,3 @@
+Nombre: Valeria Camacho Hernández
+Número de cuenta: 322007273
+Fecha: 15 de Agosto del 2024
